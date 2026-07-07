fb2d531845272223e5a0bef68d9d1ee50a8759ce
Harish <khharish2@gmail.com>
2026-07-07 17:58:45 +0100

    leetcode: 3754. Concatenate Non-Zero Digits and Multiply by Sum I

--- src/LeetCodeSolns.java
+++ src/LeetCodeSolns.java
@@ -2,7 +2,29 @@ public class LeetCodeSolns {
 
     public static void main(String[] args) {
         LeetCodeSolns leetCodeSolns = new LeetCodeSolns();
+        System.out.println(leetCodeSolns.sumAndMultiply(10203004));
+        System.out.println(leetCodeSolns.sumAndMultiply(1000));
 
+
+    }
+
+
+    public long sumAndMultiply(int n) {
+        int val = n;
+        int sum = 0;
+        String digit = "";
+        while (val != 0) {
+            int rem = val % 10;
+            if (rem != 0) {
+                digit += rem;
+                sum += rem;
+            }
+            val = val / 10;
+        }
+        if(digit.isEmpty()){
+            digit="0";
+        }
+        return Long.parseLong(new StringBuffer(digit).reverse().toString()) * sum;
     }
 
 }
